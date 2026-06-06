f42ceead5fb7484ebc21d131f911853d7b324e69
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-06 10:36:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+     public int split(int[] nums,int lim){
+        int c=1,howMuchPaint=0;
+        for(int i:nums){
+            if(i+howMuchPaint<=lim){
+                howMuchPaint+=i;
+            }else{
+                howMuchPaint=i;
+                c++;
+            }
+        }
+        return c;
+    }
+    public int splitArray(int[] arr, int k) {
+        // code here
+         int max=Integer.MIN_VALUE;
+    int high=0;
+    for(int i:arr){
+        high+=i;
+        max=Math.max(i,max);
+    }
+    int low=max;
+    int ans=0;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int cnt=split(arr,mid);
+        if(cnt>k){
+            low=mid+1;
+        }else{
+            ans=mid;
+            high=mid-1;
+        }
+    }
+    return ans;
+    }
+};
