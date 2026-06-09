5180a517b9754e3c96db6de7fe7fb6ae489acc81
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-09 19:26:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate Parentheses/generate-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void generate(int idx,String s,int open,int close,List<String> li,int n){
+        if(open>n/2) return;
+        if(open+close==n && open==close){
+            li.add(s);
+            return;
+        }
+        generate(idx+1,s+"(",open+1,close,li,n);
+        if(open>close){
+             generate(idx+1,s+")",open,close+1,li,n);
+        }
+    }
+    public ArrayList<String> generateParentheses(int n) {
+        // code here
+         ArrayList<String> li=new ArrayList<>();
+        int open=0,close=0;
+        String s="";
+        generate(0,s,open,close,li,n);
+        return li;
+    }
+}
