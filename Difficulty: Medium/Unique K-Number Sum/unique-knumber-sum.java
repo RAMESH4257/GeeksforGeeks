8613ece058803563d56c2c09b14ec1fd12fdb92d
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-11 12:25:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique K-Number Sum/unique-knumber-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sub_sequences(int sum,int last,int k,int n,ArrayList<Integer> li,ArrayList<ArrayList<Integer>> ans){
+       if(sum==0 && li.size()==k){
+        ans.add(new ArrayList<>(li));
+        return;
+       }
+       if(sum<0 || li.size()>k) return;
+       for(int i=last;i<=9;i++){
+        if(i<=sum){
+            li.add(i);
+            sub_sequences(sum-i,i+1,k,n,li,ans);
+            li.remove(li.size()-1);
+        }
+       }
+    }
+    public ArrayList<ArrayList<Integer>> combinationSum(int n, int k) {
+        // code here
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        ArrayList<Integer> li= new ArrayList<>();
+        sub_sequences(n,1,k,n,li,ans);
+        return ans;
+    }
+}
