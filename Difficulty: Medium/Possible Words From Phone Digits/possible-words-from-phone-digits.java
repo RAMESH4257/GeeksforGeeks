b018215071141a23e34530b52a6440782d3bfce7
RAMESH4257 <155468155+RAMESH4257@users.noreply.github.com>
2026-06-12 11:33:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Possible Words From Phone Digits/possible-words-from-phone-digits.java
@@ -0,0 +1,25 @@
+class Solution {
+     public static void helper(int idx,String ans,int[] arr,ArrayList<String> li){
+        if(idx==arr.length){
+            li.add(ans);
+            return;
+        }
+        String[] combo=new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        int number=arr[idx];
+        if(number==0 || number==1){
+        helper(idx+1,ans,arr,li);
+        return;
+    }
+        String temp=combo[number];
+        for(int i=0;i<temp.length();i++){
+            helper(idx+1,ans+temp.charAt(i),arr,li);
+        }
+
+    }
+    public ArrayList<String> possibleWords(int[] arr) {
+        // code here
+        ArrayList<String> li=new ArrayList<>();
+        helper(0,"",arr,li);
+        return li;
+    }
+}
